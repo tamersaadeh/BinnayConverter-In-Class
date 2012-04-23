4d7d24965fc9af6fcdef62eececcd1af36a16c21
Tamer Saadeh <tamersaadeh@gmail.com>
2012-04-23 10:37:58 +0200

    added code from class

---
+++ base2.h
@@ -0,0 +1,41 @@
+/*
+ * base2.h
+ *
+ *  Created on: 17 Apr 2012
+ *      Author: tamer
+ */
+
+void inputBinary(int *nbit, int number[], int DEBUG) {
+	do {
+		printf("Enter the number of bits for "
+				"the binary number you want to use: ");
+		int n;
+		scanf("%d", &n);
+		*nbit = n;
+		if (DEBUG && *nbit > 31)
+			printf("Error: Number of bits is too high!");
+	} while (*nbit > 31);
+
+	int i;
+	for (i = *nbit - 1; i >= 0; i--) {
+		do {
+			printf("Enter bit #%d: ", i + 1);
+			scanf("%d", &number[i]);
+			if (number[i] != 0 && number[i] != 1 && DEBUG)
+				puts("Error: Value not allowed!");
+		} while (number[i] != 0 && number[i] != 1);
+	}
+
+}
+
+int binaryToDecimal(int nbit, int number[]) {
+	int res = 0;
+
+	int pow2 = 1;
+	int i;
+	for (i = 0; i < nbit; i++) {
+		res += number[i] * pow2;
+		pow2 *= 2;
+	}
+	return res;
+}
--- main.c
+++ main.c
@@ -7,42 +7,27 @@
 
 #include <stdio.h>
 
-char* toBinarySNM(long decimal) {
-	// variables
-	static char binary[1000];
-	int k = 0, n = 0;
-	int negative = 0;
-	int remain;
-	char temp[strlen(binary)];
+#include "base2.h"
 
-	// take care of negative input
-	if (decimal < 0) {
-		decimal = -decimal;
-		negative = 1;
-	}
-
-	while (decimal > 0) {
-		remain = decimal % 2;
-		decimal = decimal / 2;
-		temp[k++] = remain + '0';
-	}
-
-	//using sign and magnitude
-
-	if (negative)
-		temp[k++] = '1';
-	else
-		temp[k++] = '0';
-
-	// reverse the spelling
-	while (k >= 0)
-		binary[n++] = temp[--k];
-
-	binary[n - 1] = 0;
-	return binary;
-}
+#define DEBUG 1
 
 int main() {
 
-}
+	if (DEBUG) {
+		int binArr[31];
+		int size;
 
+		inputBinary(&size, binArr, DEBUG);
+		int i;
+
+		printf("The number received: ");
+		for (i = size - 1; i >= 0; i--) {
+			printf("%d", binArr[i]);
+		}
+		printf("\n");
+		int decimal = binaryToDecimal(size, binArr);
+		printf("Decimal value: %d\n", decimal);
+	}
+
+	return 0;
+}
